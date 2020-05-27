60cb6f023e4f6406d00004813878c1c1e872eff0
sweetechka <titvs1337@mail.ru>
2020-05-27 14:54:16 +0700

    Added properties of counting Bulls and Cows, and history propery

--- include/Guess.h
+++ include/Guess.h
@@ -7,10 +7,17 @@ using namespace System::Collections::Generic;
 
 ref class Guess
 {
+	List<int>^ data; //Данные
+	List<int>^ trydata; //Данные попыток
+	List<String^>^ history; //История попыток
+
 public:
 
 	Guess()
 	{
+		data = gcnew List<int>(4);
+		trydata = gcnew List<int>(4);
+		history = gcnew List<String^>();
 		SetRandom();
 	}
 
@@ -21,6 +28,35 @@ public:
 		{
 			value = random->Next(1000, 10000);
 		} while (!Set(value));
+	}
+
+
+	property int Bull
+	{
+		int get() {
+			//Быки - это когда данные совпадают
+			int count = 0;
+			for (int k = 0; k < data->Count; k++)
+				if (data[k] == trydata[k]) count++;
+			return count;
+		}
+	}
+
+	property int Cow
+	{
+		int get() {
+			//Корова - это если есть в списке. Минус быки.
+			int count = 0;
+			for (int k = 0; k < trydata->Count; k++)
+				if (data->IndexOf(trydata[k]) >= 0) count++;
+			count -= Bull;
+			return count;
+		}
+	}
+
+	property List<String^>^ History
+	{
+		List<String^>^ get() { return history; }
 	}
 
 };
